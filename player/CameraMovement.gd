755985e33b27ea0eedfa312d52d62a9a55dbd8d5
drblix <benjamin800823@gmail.com>
2023-09-20 10:07:58 -0400

    first person controller finished

--- player/CameraMovement.gd
+++ player/CameraMovement.gd
@@ -4,13 +4,27 @@ extends Node
 
 @export var character_body: CharacterBody3D
 @export var camera: Camera3D
-@export var mouse_sensitivity: float = 3
 
+@export var mouse_sensitivity: float = .5
+@export var invert_mouse: bool = false
 
 func _ready():
-	print(camera)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
+		# get change in mouse position
 		var mouse_velocity: Vector2 = event.relative * mouse_sensitivity
+		# y rotation is handled by the character body
 		character_body.rotate_y(deg_to_rad(-mouse_velocity.x))
+		
+		# swap up-down rotation if inverted
+		if invert_mouse:
+			camera.rotate_x(deg_to_rad(mouse_velocity.y))
+		else:
+			camera.rotate_x(deg_to_rad(-mouse_velocity.y))
+		
+		# clamping rotation on x-axis so player can't rotate upside down
+		camera.rotation.x = clampf(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+	elif event is InputEventKey and event.keycode == KEY_ESCAPE:
+		get_tree().quit()
--- player/PlayerMovement.gd
+++ player/PlayerMovement.gd
@@ -3,12 +3,24 @@ extends Node
 @export_range(0, 25) var speed: float = 3
 
 @export var character_body: CharacterBody3D
+@export var player_camera: Camera3D
 
 
 func _physics_process(_delta: float) -> void:
-	var input_vector: Vector2 = Input.get_vector("left", "right", "down", "up")
-	var move_vector: Vector3 = Vector3(-input_vector.y, 0, -input_vector.x) * speed
+	# get input vector and convert to vector3
+	var input_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var move_vector: Vector3 = Vector3(input_vector.x, 0, input_vector.y)
+	# player moves relative to where they're facing
+	var direction: Vector3 = (character_body.transform.basis * move_vector).normalized()
 	
-	character_body.velocity = move_vector * character_body.transform.basis
+	# if not (0,0,0)
+	if direction:
+		character_body.velocity.x = direction.x * speed
+		character_body.velocity.z = direction.z * speed
+	# gradually bring the player to a halt
+	else:
+		character_body.velocity.x = move_toward(character_body.velocity.x, 0, speed)
+		character_body.velocity.z = move_toward(character_body.velocity.z, 0, speed)
+	
 	
 	character_body.move_and_slide()
