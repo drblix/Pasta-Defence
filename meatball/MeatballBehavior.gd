72450ce8dbe16e6d5bbf729a89cc93fa132e5eea
drblix <benjamin800823@gmail.com>
2023-09-25 21:52:36 -0400

    Shooting WIP

--- meatball/MeatballBehavior.gd
+++ meatball/MeatballBehavior.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 func _on_meatball_body_entered(other_body: Node):
-	if other_body.is_in_group("enemy"):
+	#await get_tree().create_timer(2).timeout
+	get_parent().queue_free()
+	#if other_body.is_in_group("enemy"):
 		# reduce hp of enemy or something
-		get_parent().queue_free()
+	#	get_parent().queue_free()
